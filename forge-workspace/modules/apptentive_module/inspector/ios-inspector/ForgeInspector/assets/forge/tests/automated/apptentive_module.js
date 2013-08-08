6c2c101d4c6dc7bc1ca69d7970d53041869a9d1b
pkamb <peterkamb@gmail.com>
2013-08-08 14:37:03 -0700

    Initial module unit tests

--- forge-workspace/modules/apptentive_module/inspector/ios-inspector/ForgeInspector/assets/forge/tests/automated/apptentive_module.js
+++ forge-workspace/modules/apptentive_module/inspector/ios-inspector/ForgeInspector/assets/forge/tests/automated/apptentive_module.js
@@ -1,13 +1,57 @@
 module("apptentive_module");
 
-// In this test we call the example showAlert API method with an empty string
-// In the example API method an empty string will immediately call the error callback
-asyncTest("Attempt to show an alert with no text", 1, function() {
-	forge.apptentive_module.showAlert("", function () {
-		ok(false, "Expected failure, got success");
-		start();
-	}, function () {
-		ok(true, "Expected failure");
-		start();
-	});
+asyncTest("Test Apptentive API key", 2, function() {
+	var myAPIKey = "940aa47d4c3eb99d07537ddf0866cefff3a37d184427cfe382a98e2fef5d55d2";
+	
+	forge.internal.call('apptentive_module.setApiKey',
+		{apiKey:myAPIKey}, 
+		function(success) {
+			ok(true, "Set API Key");
+			start();
+		},
+		function(error) {
+			ok(false, "Unable to set API key");
+			start();
+		}
+	);
+	
+	forge.internal.call('apptentive_module.apiKey',
+		{}, 
+		function(success) {
+			deepEqual(success, myAPIKey, "API Key should be set");
+			start();
+		},
+		function(error) {
+			ok(false, "Error getting API key.");
+			start();
+		}
+	);	
+});
+
+asyncTest("Test Apptentive Initial User Name", 2, function() {
+	var name = "Peter";
+	
+	forge.internal.call('apptentive_module.setInitialUserName',
+		{initialUserName:name}, 
+		function(success) {
+			ok(true, "Set user name");
+			start();
+		},
+		function(error) {
+			ok(false, "Unable to set user name");
+			start();
+		}
+	);
+	
+	forge.internal.call('apptentive_module.initialUserName',
+		{}, 
+		function(success) {
+			deepEqual(success, name, "User name should be set");
+			start();
+		},
+		function(error) {
+			ok(false, "Error getting user name.");
+			start();
+		}
+	);	
 });
