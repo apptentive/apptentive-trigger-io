18a357f93a764510c054ce9883f5a9b08fe15496
pkamb <peterkamb@gmail.com>
2014-10-07 12:23:40 -0700

    Update bindings for 1.5.8

--- forge-workspace/modules/apptentive/inspector/ios-inspector/ForgeModule/apptentive_API.h
+++ forge-workspace/modules/apptentive/inspector/ios-inspector/ForgeModule/apptentive_API.h
@@ -13,6 +13,54 @@
 {}
 #pragma mark - Apptentive Shared Features
 
+///---------------------------------
+/// @name Basic Usage
+///---------------------------------
+
+/**
+ API key and App Store ID are set automatically from module config.
+ */
+
+///---------------------------------
+/// @name Interface Customization
+///---------------------------------
+
++ (void)setShowEmailField:(ForgeTask *)task showEmailField:(NSNumber *)showEmailField;
+
++ (void)setCustomPlaceholderText:(ForgeTask *)task customPlaceholderText:(NSString *)customPlaceholderText;
+
++ (void)setInitiallyUseMessageCenter:(ForgeTask *)task initiallyUseMessageCenter:(NSNumber *)initiallyUseMessageCenter;
+
++ (void)setInitiallyHideBranding:(ForgeTask *)task initiallyHideBranding:(NSNumber *)initiallyHideBranding;
+
++ (void)setTintColor:(ForgeTask *)task red:(NSNumber *)red green:(NSNumber *)green blue:(NSNumber *)blue alpha:(NSNumber *)alpha;
+
+///--------------------
+/// @name Presenting UI
+///--------------------
+
++ (void)showMessageCenter:(ForgeTask *)task;
++ (void)showMessageCenterWithCustomData:(ForgeTask *)task customData:(NSDictionary *)customData;
++ (void)getUnreadMessageCount:(ForgeTask *)task;
+
++ (void)engage:(ForgeTask *)task event:(NSString *)event;
++ (void)engage:(ForgeTask *)task event:(NSString *)event customData:(NSDictionary *)customData;
++ (void)engage:(ForgeTask *)task event:(NSString *)event customData:(NSDictionary *)customData extendedData:(NSArray *)extendedData;
+
+///-------------------------------
+/// @name Extended Data for Events
+///-------------------------------
+
+
+///-------------------------------------
+/// @name Attach Text, Images, and Files
+///-------------------------------------
+
+
+///---------------------------------------
+/// @name Add Custom Device or Person Data
+///---------------------------------------
+
 + (void)setInitialUserName:(ForgeTask *)task initialUserName:(NSString *)initialUserName;
 + (void)setInitialUserEmailAddress:(ForgeTask *)task initialUserEmailAddress:(NSString *)initialUserEmailAddress;
 
@@ -22,14 +70,13 @@
 + (void)addCustomDeviceData:(ForgeTask *)task key:(NSString *)key value:(NSObject<NSCoding> *)value;
 + (void)removeCustomDeviceData:(ForgeTask *)task key:(NSString *)key;
 
-#pragma mark - Message Center
+///---------------------------------
+/// @name Open App Store
+///---------------------------------
 
-+ (void)showMessageCenter:(ForgeTask *)task;
-+ (void)showMessageCenterWithCustomData:(ForgeTask *)task customData:(NSDictionary *)customData;
-+ (void)getUnreadMessageCount:(ForgeTask *)task;
 
-#pragma mark - Engagement
-
-+ (void)engage:(ForgeTask *)task codePoint:(NSString *)codePoint;
+///------------------------------------
+/// @name Integrate With Other Services
+///------------------------------------
 
 @end
--- forge-workspace/modules/apptentive/inspector/ios-inspector/ForgeModule/apptentive_API.m
+++ forge-workspace/modules/apptentive/inspector/ios-inspector/ForgeModule/apptentive_API.m
@@ -14,6 +14,92 @@
 {}
 #pragma mark - Apptentive Shared Features
 
+///---------------------------------
+/// @name Basic Usage
+///---------------------------------
+
+/**
+ API key and App Store ID are set automatically from module config.
+ */
+
+///---------------------------------
+/// @name Interface Customization
+///---------------------------------
+
++ (void)setShowEmailField:(ForgeTask *)task showEmailField:(NSNumber *)showEmailField {
+    [[ATConnect sharedConnection] setShowEmailField:[showEmailField boolValue]];
+    [task success:nil];
+}
+
++ (void)setCustomPlaceholderText:(ForgeTask *)task customPlaceholderText:(NSString *)customPlaceholderText {
+    [[ATConnect sharedConnection] setCustomPlaceholderText:customPlaceholderText];
+    [task success:nil];
+}
+
++ (void)setInitiallyUseMessageCenter:(ForgeTask *)task initiallyUseMessageCenter:(NSNumber *)initiallyUseMessageCenter {
+    [[ATConnect sharedConnection] setInitiallyUseMessageCenter:[initiallyUseMessageCenter boolValue]];
+    [task success:nil];
+}
+
++ (void)setInitiallyHideBranding:(ForgeTask *)task initiallyHideBranding:(NSNumber *)initiallyHideBranding {
+    [[ATConnect sharedConnection] setInitiallyHideBranding:[initiallyHideBranding boolValue]];
+    [task success:nil];
+}
+
++ (void)setTintColor:(ForgeTask *)task red:(NSNumber *)red green:(NSNumber *)green blue:(NSNumber *)blue alpha:(NSNumber *)alpha  {
+    UIColor *tintColor = [UIColor colorWithRed:[red floatValue] green:[green floatValue] blue:[blue floatValue] alpha:[alpha floatValue]];
+    [[ATConnect sharedConnection] setTintColor:tintColor];
+    [task success:nil];
+}
+
+///--------------------
+/// @name Presenting UI
+///--------------------
+
++ (void)showMessageCenter:(ForgeTask *)task {
+    [[ATConnect sharedConnection] presentMessageCenterFromViewController:[[ForgeApp sharedApp] viewController]];
+    [task success:nil];
+}
+
++ (void)showMessageCenterWithCustomData:(ForgeTask *)task customData:(NSDictionary *)customData {
+    [[ATConnect sharedConnection] presentMessageCenterFromViewController:[[ForgeApp sharedApp] viewController] withCustomData:customData];
+    [task success:nil];
+}
+
++ (void)getUnreadMessageCount:(ForgeTask *)task {
+    NSUInteger unreadMessageCount = [[ATConnect sharedConnection] unreadMessageCount];
+    [task success:[NSNumber numberWithInteger:unreadMessageCount]];
+}
+
++ (void)engage:(ForgeTask *)task event:(NSString *)event {
+    BOOL didEngage = [[ATConnect sharedConnection] engage:event fromViewController:[[ForgeApp sharedApp] viewController]];
+    [task success:@(didEngage)];
+}
+
++ (void)engage:(ForgeTask *)task event:(NSString *)event customData:(NSDictionary *)customData {
+    BOOL didEngage = [[ATConnect sharedConnection] engage:event withCustomData:customData fromViewController:[[ForgeApp sharedApp] viewController]];
+    [task success:@(didEngage)];
+}
+
++ (void)engage:(ForgeTask *)task event:(NSString *)event customData:(NSDictionary *)customData extendedData:(NSArray *)extendedData {
+    BOOL didEngage = [[ATConnect sharedConnection] engage:event withCustomData:customData withExtendedData:extendedData fromViewController:[[ForgeApp sharedApp] viewController]];
+    [task success:@(didEngage)];
+}
+
+///-------------------------------
+/// @name Extended Data for Events
+///-------------------------------
+
+
+///-------------------------------------
+/// @name Attach Text, Images, and Files
+///-------------------------------------
+
+
+///---------------------------------------
+/// @name Add Custom Device or Person Data
+///---------------------------------------
+
 + (void)setInitialUserName:(ForgeTask *)task initialUserName:(NSString *)initialUserName
 {
     [[ATConnect sharedConnection] setInitialUserName:initialUserName];
@@ -50,32 +136,13 @@
     [task success:nil];
 }
 
-#pragma mark - Message Center
+///---------------------------------
+/// @name Open App Store
+///---------------------------------
 
-+ (void)showMessageCenter:(ForgeTask *)task
-{
-    [[ATConnect sharedConnection] presentMessageCenterFromViewController:[[ForgeApp sharedApp] viewController]];
-    [task success:nil];
-}
 
-+ (void)showMessageCenterWithCustomData:(ForgeTask *)task customData:(NSDictionary *)customData
-{
-    [[ATConnect sharedConnection] presentMessageCenterFromViewController:[[ForgeApp sharedApp] viewController] withCustomData:customData];
-    [task success:nil];
-}
-
-+ (void)getUnreadMessageCount:(ForgeTask *)task
-{
-    NSUInteger unreadMessageCount = [[ATConnect sharedConnection] unreadMessageCount];
-    [task success:[NSNumber numberWithInteger:unreadMessageCount]];
-}
-
-#pragma mark - Engagement
-
-+ (void)engage:(ForgeTask *)task codePoint:(NSString *)codePoint
-{
-    [[ATConnect sharedConnection] engage:codePoint fromViewController:[[ForgeApp sharedApp] viewController]];
-    [task success:nil];
-}
+///------------------------------------
+/// @name Integrate With Other Services
+///------------------------------------
 
 @end
