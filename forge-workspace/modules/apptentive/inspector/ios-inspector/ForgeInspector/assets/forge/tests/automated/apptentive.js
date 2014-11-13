e6e50f41452b537a205370eada9f5663dac32e57
pkamb <peterkamb@gmail.com>
2014-11-13 14:54:06 -0800

    Update iOS Inspector.

--- forge-workspace/modules/apptentive/inspector/ios-inspector/ForgeInspector/assets/forge/tests/automated/apptentive.js
+++ forge-workspace/modules/apptentive/inspector/ios-inspector/ForgeInspector/assets/forge/tests/automated/apptentive.js
@@ -1,32 +1,6 @@
 module("apptentive");
 
-var myAPIKey = "940aa47d4c3eb99d07537ddf0866cefff3a37d184427cfe382a98e2fef5d55d2";
-asyncTest("Test setting API key", 1, function() {
-	forge.apptentive.setApiKey(myAPIKey,
-		function(success) {
-			ok(true, "Set API Key");
-			start();
-		},
-		function(error) {
-			ok(false, "Unable to set API key");
-			start();
-		}
-	);
-});
-
-asyncTest("Test getting API key", 1, function() {
-	forge.apptentive.apiKey(
-		function(success) {
-			deepEqual(success, myAPIKey, "API Key should be set");
-			start();
-		},
-		function(error) {
-			ok(false, "Error getting API key.");
-			start();
-		}
-	);
-});
-
+// This automated test script uses the QUnit API: http://api.qunitjs.com/category/assert/
 
 var name = "Peter";
 asyncTest("Test setting Apptentive Initial User Name", 1, function() {
@@ -42,18 +16,20 @@ asyncTest("Test setting Apptentive Initial User Name", 1, function() {
 	);
 });
 
-asyncTest("Test getting Apptentive Initial User Name", 1, function() {
-	forge.apptentive.initialUserName(
-		function(success) {
-			deepEqual(success, name, "User name should be set");
-            start();
-		},
-		function(error) {
-			ok(false, "Error getting user name.");
-			start();
-		}
-	);
-});
+if (forge.is.ios()) {
+	asyncTest("Test getting Apptentive Initial User Name", 1, function() {
+		forge.apptentive.initialUserName(
+			function(success) {
+				deepEqual(success, name, "User name should be set");
+				start();
+			},
+			function(error) {
+				ok(false, "Error getting user name.");
+				start();
+			}
+		);
+	});
+}
 
 var email = "peter@apptentive.com";
 asyncTest("Test setting Apptentive Initial User Email Address", 1, function() {
@@ -69,49 +45,70 @@ asyncTest("Test setting Apptentive Initial User Email Address", 1, function() {
 	);
 });
 
-asyncTest("Test getting Apptentive Initial User Email Address", 1, function() {
-	forge.apptentive.initialUserEmailAddress(
+var customDeviceDataKey = "city";
+var customDeviceDataValue = "Seattle";
+asyncTest("Test adding custom device data", 1, function() {
+	forge.apptentive.addCustomDeviceData(
 		function(success) {
-			deepEqual(success, email, "User email address should be set");
-            start();
+			ok(true, "Added custom device data");
+			start();
 		},
 		function(error) {
-			ok(false, "Error getting user email address.");
+			ok(false, "Unable to add custom device data");
 			start();
-		}
+		},
+		customDeviceDataKey,
+		customDeviceDataValue
 	);
 });
 
-var dataObject = "Seattle";
-var dataKey = "city";
-asyncTest("Test setting custom data", 1, function() {
-	forge.apptentive.addCustomData(dataObject, dataKey,
+asyncTest("Test removing custom device data", 1, function() {
+	forge.apptentive.removeCustomDeviceData(
 		function(success) {
-			ok(true, "Set custom data");
+			ok(true, "Removed custom device data");
 			start();
 		},
 		function(error) {
-			ok(false, "Unable to set custom data");
+			ok(false, "Unable to remove custom device data");
 			start();
-		}
+		},
+		customDeviceDataKey
 	);
 });
 
-asyncTest("Test removing custom data", 1, function() {
-	forge.apptentive.removeCustomData(dataKey,
+var customPersonDataKey = "user-id";
+var customPersonDataValue = "123456789";
+asyncTest("Test adding custom person data", 1, function() {
+	forge.apptentive.addCustomPersonData(
 		function(success) {
-			ok(true, "Removed custom data");
+			ok(true, "Added custom person data");
 			start();
 		},
 		function(error) {
-			ok(false, "Unable to remove custom data");
+			ok(false, "Unable to add custom person data");
 			start();
-		}
+		},
+		customPersonDataKey,
+		customPersonDataValue
+	);
+});
+
+asyncTest("Test removing custom person data", 1, function() {
+	forge.apptentive.removeCustomPersonData(
+		function(success) {
+			ok(true, "Removed custom person data");
+			start();
+		},
+		function(error) {
+			ok(false, "Unable to remove custom person data");
+			start();
+		},
+		customPersonDataKey
 	);
 });
 
 asyncTest("Test unread message count", 1, function() {
-	forge.apptentive.unreadMessageCount(
+	forge.apptentive.getUnreadMessageCount(
 		function(success) {
 			ok(true, "Unread messages: " + success);
 			start();
@@ -123,68 +120,207 @@ asyncTest("Test unread message count", 1, function() {
 	);
 });
 
-var myAppID = 343200656;
-asyncTest("Test setting app ID", 1, function() {
-	forge.apptentive.setAppID(myAppID,
+asyncTest("Test engage(\"aaa\"). Should return false.", 1, function() {
+	forge.apptentive.engage(
 		function(success) {
-			ok(true, "Set App ID");
+			forge.logging.info("engage() returned: " + (typeof success) + " -> " + success);
+			equal(success, false, "Showed Interaction? " + success);
 			start();
 		},
 		function(error) {
-			ok(false, "Unable to set App ID");
+			ok(false, "Error: " + error);
 			start();
+		},
+		"aaa"
+	);
+});
+
+asyncTest("Test engage(\"event_1\"). Should return true.", 1, function() {
+	forge.apptentive.engage(
+		function(success) {
+			forge.logging.info("engage() returned: " + (typeof success) + " -> " + success);
+			equal(success, true, "Showed Interaction? " + success);
+			start();
+		},
+		function(error) {
+			ok(false, "Error: " + error);
+			start();
+		},
+		"event_1"
+	);
+});
+
+asyncTest("Test engage(\"event_2\") with custom data. Should return true.", 1, function() {
+	forge.apptentive.engage(
+		function(success) {
+			forge.logging.info("engage() returned: " + (typeof success) + " -> " + success);
+			equal(success, true, "Showed Interaction? " + success);
+			start();
+		},
+		function(error) {
+			ok(false, "Error: " + error);
+			start();
+		},
+		"event_2",
+		{
+			"foo": "bar"
 		}
 	);
 });
 
-asyncTest("Test getting app ID", 1, function() {
-	forge.apptentive.appID(
+asyncTest("Test engage(\"event_3\") with custom data and extendedData. Should return true.", 1, function() {
+	forge.apptentive.engage(
 		function(success) {
-			deepEqual(success, myAppID, "App ID should be set");
+			forge.logging.info("engage() returned: " + (typeof success) + " -> " + success);
+			equal(success, true, "Showed Interaction? " + success);
 			start();
 		},
 		function(error) {
-			ok(false, "Unable to get App ID");
+			ok(false, "Error: " + error);
 			start();
-		}
+		},
+		"event_3",
+		{
+			"string": "bar",
+			"number": 1234567890
+		},
+		[
+			{
+				"time": {
+					"version": 1,
+					"timestamp": 1.406251926165E9
+				}
+			},
+			{
+				"location": {
+					"version": 1,
+					"coordinates": [
+						-122.34569190000002,
+						47.6288591
+					]
+				}
+			},
+			{
+				"commerce": {
+					"version": 1,
+					"id": "commerce_id",
+					"affiliation": 1111111111,
+					"revenue": 100,
+					"shipping": 5,
+					"tax": 4.38,
+					"currency": "USD",
+					"items": [
+						{
+							"id": 22222222,
+							"name": "Item Name",
+							"category": "Category",
+							"price": 20,
+							"quantity": 5,
+							"currency": "USD"
+						}
+					]
+				}
+			}
+		]
 	);
 });
 
-asyncTest("Test logging significant event", 1, function() {
-	forge.apptentive.logSignificantEvent(
+asyncTest("Test engage(\"event_3\") with empty custom data and complete extendedData. Should return true.", 1, function() {
+	forge.apptentive.engage(
 		function(success) {
-			ok(true, "logged significant event.");
+			forge.logging.info("engage() returned: " + (typeof success) + " -> " + success);
+			equal(success, true, "Showed Interaction? " + success);
 			start();
 		},
 		function(error) {
-			ok(false, "Unable to log significant event.");
+			ok(false, "Error: " + error);
 			start();
-		}
+		},
+		"event_3",
+		{},
+		[
+			{
+				"time": {
+					"version": 1,
+					"timestamp": 1.406251926165E9
+				}
+			},
+			{
+				"location": {
+					"version": 1,
+					"coordinates": [
+						-122.34569190000002,
+						47.6288591
+					]
+				}
+			},
+			{
+				"commerce": {
+					"version": 1,
+					"id": "commerce_id",
+					"affiliation": 1111111111,
+					"revenue": 100,
+					"shipping": 5,
+					"tax": 4.38,
+					"currency": "USD",
+					"items": [
+						{
+							"id": 22222222,
+							"name": "Item Name",
+							"category": "Category",
+							"price": 20,
+							"quantity": 5,
+							"currency": "USD"
+						}
+					]
+				}
+			}
+		]
 	);
 });
 
-asyncTest("Test getting surveys with no tags", 1, function() {
-	forge.apptentive.hasSurveyAvailableWithNoTags(
+asyncTest("Test makeExtendedDataTime().", 1, function() {
+	forge.apptentive.makeExtendedDataTime(
 		function(success) {
-			ok(true, "Has " + success + " surveys with no tags available");
+			ok(true, "Did it worked?");
 			start();
 		},
 		function(error) {
-			ok(false, "Unable to get surveys with no tags");
+			ok(false, "Error: " + error);
 			start();
-		}
+		},
+		new Date().getTime()
 	);
 });
 
-asyncTest("Test getting surveys with tags", 1, function() {
-	forge.apptentive.hasSurveyAvailableWithTags(["tag", "testTag", "testing"],
+
+asyncTest("Test engageWithExtendedDataEndToEnd().", 1, function() {
+	var timeExtendedData;
+	forge.apptentive.makeExtendedDataTime(
 		function(success) {
-			ok(true, "Has " + success + " surveys with tags available");
-			start();
+			timeExtendedData = success;
+			forge.logging.info("TimeExtendedData: " + timeExtendedData);
+
+			forge.apptentive.engage(
+				function(success) {
+					forge.logging.info("engage() returned: " + (typeof success) + " -> " + success);
+					equal(success, true, "Showed Interaction? " + success);
+					start();
+				},
+				function(error) {
+					ok(false, "Error: " + error);
+					start();
+				},
+				"event_1",
+				{},
+				[timeExtendedData]
+			);
 		},
 		function(error) {
-			ok(false, "Unable to get surveys with tags");
+			ok(false, "Error: " + error);
 			start();
-		}
+		},
+		new Date().getTime()
 	);
 });
+
